c8a294a19f4b710d7c285e42db55bcb5bcc00b0b
chandini kurukunti <chandukurukunti72@gmail.com>
2025-09-10 21:00:47 +0530

    merge files

---
+++ mathematical operations.py
@@ -0,0 +1,7 @@
+
+a = float (input ("enter the first number:"))
+b = float(input ("enter the second number:" )) 
+print ("addition" , a+b)
+print("subtraction", a - b)
+print("multiplication", a*b)
+print("division", a/b)
